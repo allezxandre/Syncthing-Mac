e26f188b31fa3a8fa15d597f3b6e249637c9431c
Alexandre Jouandin <alexandre@jouand.in>
2015-05-14 17:20:18 +0200

    Initial Commit

---
+++ Syncthing GUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Syncthing GUI
+//
+//  Created by Alexandre Jouandin on 2015/05/14.
+//  Copyright (c) 2015 Alexandre Jouandin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Syncthing GUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Syncthing GUI
+//
+//  Created by Alexandre Jouandin on 2015/05/14.
+//  Copyright (c) 2015 Alexandre Jouandin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
